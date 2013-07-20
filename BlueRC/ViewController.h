8a6b488af0e713929145dee6805a4190ce3e6d64
Scott Cheezem <scott.cheezem@gmail.com>
2013-07-20 13:13:34 -0400

    Initial Commit

---
+++ BlueRC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlueRC
+//
+//  Created by user on 7/20/13.
+//  Copyright (c) 2013 Scott Cheezem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BlueRC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlueRC
+//
+//  Created by user on 7/20/13.
+//  Copyright (c) 2013 Scott Cheezem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
